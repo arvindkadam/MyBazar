456b2d4045350783f37dda5508c3dab96dd35a27
Arvind Kadam <arvindkadam95@gmail.com>
2019-01-23 22:34:43 +0530

    CRUD Operations Done

---
+++ MyBazar/MyBazar.Services/ProductsService.cs
@@ -0,0 +1,60 @@
+﻿using MyBazar.Database;
+using MyBazar.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyBazar.Services
+{
+   public class ProductsService:DbContext,IDisposable
+    {
+
+        public List<Product> GetProducts()
+        {
+            using(var context = new MyBazarContext())
+            {
+                return context.Products.ToList();
+            }
+        }
+
+        public void SaveProduct(Product product)
+        {
+            using(var context = new MyBazarContext())
+            {
+                context.Products.Add(product);
+                context.SaveChanges();
+            }
+        }
+        public Product GetProduct(int ID)
+        {
+            using (var context = new MyBazarContext())
+            {
+                return context.Products.Find(ID);
+            }
+        }
+        public void UpdateProduct(Product product)
+        {
+            using (var context = new MyBazarContext())
+            {
+                context.Entry(product).State = System.Data.Entity.EntityState.Modified;
+                context.SaveChanges();
+            }
+        }
+        public void DeleteProduct(int ID)
+        {
+
+            using (var context = new MyBazarContext())
+            {
+                var product = context.Products.Find(ID);
+                //context.Entry(product).State = System.Data.Entity.EntityState.Deleted;
+                context.Products.Remove(product);
+                context.SaveChanges();
+            }
+        }
+
+
+    }
+}
---
+++ MyBazar/MyBazar.Web/Controllers/ProductController.cs
@@ -0,0 +1,67 @@
+﻿using MyBazar.Entities;
+using MyBazar.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyBazar.Web.Controllers
+{
+    public class ProductController : Controller
+    {
+        ProductsService ProductService = new ProductsService();
+        // GET: Product
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ProductsTable(string search)
+        {
+            var products = ProductService.GetProducts();
+            if (!string.IsNullOrEmpty(search))
+            {
+                products = products.Where(x =>x.Name !=null && x.Name.ToLower().Contains(search.ToLower())).ToList();
+            }
+           
+            return PartialView(products);
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return PartialView();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Product product)
+        {
+            ProductService.SaveProduct(product);
+            return RedirectToAction("ProductsTable");
+        }
+
+        [HttpGet]
+        public ActionResult Edit(int ID)
+        {
+            var product = ProductService.GetProduct(ID);
+                return View(product);
+        }
+        [HttpPost]
+        public ActionResult Edit(Product product)
+        {
+            ProductService.UpdateProduct(product);
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public ActionResult Delete(Product  product)
+        {
+            product = ProductService.GetProduct(product.ID);
+            ProductService.DeleteProduct(product.ID);
+
+            return RedirectToAction("Index");
+        }
+    }
+}
